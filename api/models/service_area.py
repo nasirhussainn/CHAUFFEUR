d241934796f1991f75c52547afb71b3dbc25394b
Nasir <nasirhussaintormik@gmail.com>
2025-06-28 16:39:01 +0500

    Add ServiceArea model, serializer, and viewset; implement image handling on delete and update

--- api/models/service_area.py
+++ api/models/service_area.py
@@ -1,8 +1,33 @@
 from django.db import models
+import os
+from django.dispatch import receiver
 
 class ServiceArea(models.Model):
     area_name = models.CharField(max_length=255)
     description = models.TextField()
     image1 = models.ImageField(upload_to='service_areas/')
-    image2 = models.ImageField(upload_to='service_areas/')
+    image2 = models.ImageField(upload_to='service_areas/', null=True, blank=True)
     content = models.JSONField()
+
+# Delete files on object delete
+@receiver(models.signals.post_delete, sender=ServiceArea)
+def delete_service_area_images_on_delete(sender, instance, **kwargs):
+    for field in ['image1', 'image2']:
+        image = getattr(instance, field)
+        if image and os.path.isfile(image.path):
+            os.remove(image.path)
+
+# Delete old files on update
+@receiver(models.signals.pre_save, sender=ServiceArea)
+def delete_old_service_area_images_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+    try:
+        old = ServiceArea.objects.get(pk=instance.pk)
+    except ServiceArea.DoesNotExist:
+        return
+    for field in ['image1', 'image2']:
+        old_file = getattr(old, field)
+        new_file = getattr(instance, field)
+        if old_file and old_file != new_file and os.path.isfile(old_file.path):
+            os.remove(old_file.path)
--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,15 @@ from api.views.landing import landing_view
 from api.views.booking_view import BookingViewSet
 from api.views.type_of_ride_view import TypeOfRideViewSet
 from api.views.service_view import ServiceViewSet
+from api.views.service_area_view import ServiceAreaViewSet
 
 router = DefaultRouter()
 router.register(r'vehicles', VehicleViewSet, basename='vehicle')
 router.register(r'bookings', BookingViewSet, basename='booking')
 router.register(r'type-of-ride', TypeOfRideViewSet, basename='type-of-ride')
 router.register(r'services', ServiceViewSet, basename='services')
+router.register(r'service-areas', ServiceAreaViewSet, basename='service-areas')
+
 
 urlpatterns = [
     path('', landing_view, name='landing'),
