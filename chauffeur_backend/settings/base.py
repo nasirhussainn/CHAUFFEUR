0dbf43685f383efdd99d07f42f3162561be10cf6
Nasir <nasirhussaintormik@gmail.com>
2025-07-03 13:47:40 +0500

    Add CORS headers middleware and enable all origins in settings

--- chauffeur_backend/settings/base.py
+++ chauffeur_backend/settings/base.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 DATABASES = {
@@ -33,6 +34,8 @@ DATABASES = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,7 +144,7 @@ DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 CUSTOM_IMAGE_PORT = env.int('CUSTOM_PORT')
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
