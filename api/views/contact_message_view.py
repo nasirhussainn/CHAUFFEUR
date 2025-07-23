6b94b8635ed4c687c8411f2a68011ff37772d0d2
Nasir <nasirhussaintormik@gmail.com>
2025-07-23 20:09:41 +0500

    Refactor ContactMessageViewSet to handle email sending and response messages in create method

--- api/views/contact_message_view.py
+++ api/views/contact_message_view.py
@@ -10,45 +10,48 @@ class ContactMessageViewSet(viewsets.ModelViewSet):
     queryset = ContactMessage.objects.all()
     serializer_class = ContactMessageSerializer
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         contact = serializer.save()
 
-        # --- Admin Email ---
-        admin_subject = "New Contact Message"
-        admin_html = render_to_string("emails/contact_email.html", {
+        # Send emails
+        self._send_admin_email(contact)
+        self._send_user_email(contact)
+
+        response = super().create(request, *args, **kwargs)
+        return Response({
+            "message": "Your message has been received successfully.",
+            "data": response.data
+        }, status=response.status_code)
+
+    def _send_admin_email(self, contact):
+        subject = "New Contact Message"
+        html = render_to_string("emails/contact_email.html", {
             "first_name": contact.first_name,
             "last_name": contact.last_name,
             "email": contact.email,
             "phone": contact.phone,
             "message": contact.message
         })
-        admin_email = EmailMessage(
-            subject=admin_subject,
-            body=admin_html,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            to=[settings.CONTACT_RECEIVER_EMAIL]
-        )
-        admin_email.content_subtype = "html"
-        admin_email.send()
+        email = EmailMessage(subject, html, settings.DEFAULT_FROM_EMAIL, [settings.CONTACT_RECEIVER_EMAIL])
+        email.content_subtype = "html"
+        email.send()
 
-        # --- User Confirmation Email ---
-        user_subject = "We Received Your Message"
-        user_html = render_to_string("emails/contact_confirmation_email.html", {
+    def _send_user_email(self, contact):
+        subject = "We Received Your Message"
+        html = render_to_string("emails/contact_confirmation_email.html", {
             "first_name": contact.first_name,
             "last_name": contact.last_name,
             "message": contact.message
         })
-        user_email = EmailMessage(
-            subject=user_subject,
-            body=user_html,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            to=[contact.email]
-        )
-        user_email.content_subtype = "html"
-        user_email.send()
+        email = EmailMessage(subject, html, settings.DEFAULT_FROM_EMAIL, [contact.email])
+        email.content_subtype = "html"
+        email.send()
 
     def destroy(self, request, *args, **kwargs):
-        response = super().destroy(request, *args, **kwargs)
+        super().destroy(request, *args, **kwargs)
         return Response({
-            "message": "Contact message deleted successfully.",
+            "success": True,
+            "message": "Contact message deleted successfully."
         }, status=status.HTTP_200_OK)
