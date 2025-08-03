e8af7f74929b98d80727c22ba74d45ebbd64b7da
Nasir <nasirhussaintormik@gmail.com>
2025-08-03 11:29:34 +0500

    Make email and phone_number fields optional in Passenger model

--- api/models/passenger.py
+++ api/models/passenger.py
@@ -4,6 +4,6 @@ from api.models.booking import Booking
 class Passenger(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='passengers')
